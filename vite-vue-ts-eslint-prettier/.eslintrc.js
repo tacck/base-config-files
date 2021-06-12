b3750a8b87eb476eedfbe66e9f364022ef22f0a0
Kihara, Takuya <gray@tacck.net>
2021-06-12 14:24:52 +0000

    add vite-vue-ts-eslint-prettier

---
+++ vite-vue-ts-eslint-prettier/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'only-multiline'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ vite-vue-ts-eslint-prettier/README.md
@@ -0,0 +1,7 @@
+# js-eslint-prettier
+
+Vue.js + ESLint + Pretteir で Vue CLI のプロジェクト作成状態から。
+
+```
+$ curl https://raw.githubusercontent.com/tacck/base-config-files/main/vue-eslint-prettier/download.sh | sh
+```
