f3b287907ace60aa92760e21cb84dc0774bf4d6b
Kihara, Takuya <gray@tacck.net>
2021-07-20 11:24:14 +0900

    update extends and parserOptions

--- typescript-eslint-prettier/.eslintrc.js
+++ typescript-eslint-prettier/.eslintrc.js
@@ -10,17 +10,13 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2021,
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
