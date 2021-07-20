d4096c02cf32893368eaff7ffc25de9ddf27b604
Kihara, Takuya <gray@tacck.net>
2021-07-20 02:04:38 +0000

    typescript-eslint-prettier

---
+++ typescript-eslint-prettier/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'only-multiline'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ typescript-eslint-prettier/README.md
@@ -0,0 +1,9 @@
+# typescript-eslint-prettier
+
+```
+yarn add -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier
+```
+
+```
+curl https://raw.githubusercontent.com/tacck/base-config-files/main/typescript-eslint-prettier/download.sh | sh
+```
