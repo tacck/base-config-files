282c82c6a5cf42c3200b2325fd087705f03bcbc7
Kihara, Takuya <gray@tacck.net>
2021-10-05 04:20:28 +0000

    update clasp-typescript-eslint-prettier

--- clasp-typescript-eslint-prettier/download.sh
+++ clasp-typescript-eslint-prettier/download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 BASE_URL="https://raw.githubusercontent.com/tacck/base-config-files/main"
-TARGET_SET=$(basename $(pwd))
+TARGET_SET="clasp-typescript-eslint-prettier"
 CURL_OPT="-s"
 FILE_LIST="files.txt"
 
 mkdir -p .vscode src dist
 
-curl ${FILE_LIST}
+curl -O ${BASE_URL}/${TARGET_SET}/${FILE_LIST}
 
 for file_name in $(cat ${FILE_LIST}); do
-  CURL_OPT+=" -o ${f} ${BASE_URL}/${TARGET_SET}/${f}"
+  CURL_OPT+=" -o ${file_name} ${BASE_URL}/${TARGET_SET}/${file_name}"
 done
 
 curl ${CURL_OPT}
